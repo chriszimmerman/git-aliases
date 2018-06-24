3aecd004b7e314b97db228b070de8cc96b7eefe9
Chris Zimmerman <chris@chriszimmerman.net>
2018-06-24 12:02:36 -0400

    Adds common git aliases

---
+++ add-git-aliases.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git config --global alias.st 'status'
+git config --global alias.cout 'checkout'
+git config --global alias.coutb 'checkout -b'
+git config --global alias.amend 'commit -a --amend'
+git config --global alias.delete 'branch -D'
+git config --global alias.fpull '!git fetch && git pull'
